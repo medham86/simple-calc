a5c804cefd0df3ad6388dd69273e16b88d13f3ad
medhat <eng.medhathamada@gmail.com>
2025-09-09 22:13:53 +0300

    SC1-3 create new test file

---
+++ src/app/test.tsx
@@ -0,0 +1,15 @@
+
+import React from "react";
+import { Text, View } from "react-native";
+
+
+export default function Page() {
+    return (
+        <View className="flex flex-1">
+            <Text>Hello</Text>
+
+        </View>
+    );
+}
+
+
